0a6f190134c0f7418712c0e336230ba586ec1006
HAEOII <155179713+HAEOII@users.noreply.github.com>
2024-04-11 17:49:00 +0900

    로그인 전 페이지1차

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
+import LoginBt from "./img/LoginBt.png";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <div>
+      <div className="full-background">
+        <div className="Mainpagetotal">
+          <div className="header">
+            <div>
+              The Bird fight its way out of the egg. <br></br>The egg is the
+              world.<br></br> Who would be born must<br></br> first destory a
+              world.
+            </div>
+            <div>
+              {" "}
+              <img src={LoginBt} alt="LoginBt" />{" "}
+            </div>
+          </div>
+          <div className="MainText">PEG</div>
+        </div>
+      </div>
+      <div className="full-backgroundd"></div>
+    </div>
+  );
+};
 
 export default App;
