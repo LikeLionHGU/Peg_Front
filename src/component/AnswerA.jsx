9b88c7b5ae8f0677a00c1d270de1948462cbc19c
junglesub <junglesubmarine@gmail.com>
2024-06-08 13:37:29 +0900

    feat: ox

--- src/component/AnswerA.jsx
+++ src/component/AnswerA.jsx
@@ -4,6 +4,13 @@ function AnswerA({ nextQuestion, setAnswer, questionId, currentAnswer }) {
   return (
     <div>
       <div
+        style={{
+          margin: "4px",
+          cursor: "pointer",
+          border: "1px solid white",
+          padding: "16px 40px",
+          borderRadius: 10,
+        }} // 추가된 key prop
         className={currentAnswer === "O" ? "selected" : ""}
         onClick={() => {
           setAnswer(questionId, "O");
@@ -13,6 +20,14 @@ function AnswerA({ nextQuestion, setAnswer, questionId, currentAnswer }) {
         O
       </div>
       <div
+        style={{
+          margin: "4px",
+          cursor: "pointer",
+          border: "1px solid white",
+          padding: "16px 40px",
+          marginBottom: 100,
+          borderRadius: 10,
+        }} // 추가된 key prop
         className={currentAnswer === "X" ? "selected" : ""}
         onClick={() => {
           setAnswer(questionId, "X");
@@ -26,3 +41,22 @@ function AnswerA({ nextQuestion, setAnswer, questionId, currentAnswer }) {
 }
 
 export default AnswerA;
+
+// <div
+//   className={currentAnswer === "O" ? "selected" : ""}
+//   onClick={() => {
+//     setAnswer(questionId, "O");
+//     nextQuestion();
+//   }}
+// >
+//   O
+// </div>
+// <div
+//   className={currentAnswer === "X" ? "selected" : ""}
+//   onClick={() => {
+//     setAnswer(questionId, "X");
+//     nextQuestion();
+//   }}
+// >
+//   X
+// </div>
